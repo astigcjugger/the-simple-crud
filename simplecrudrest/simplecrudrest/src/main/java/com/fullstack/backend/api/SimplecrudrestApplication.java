d4dff70275f2fbafa16d89cf540f7a290ebf60be
astigcjugger <itsmegerryt@gmail.com>
2020-02-07 06:15:33 -0800

    Initial commit.

---
+++ simplecrudrest/simplecrudrest/src/main/java/com/fullstack/backend/api/SimplecrudrestApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecrudrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecrudrestApplication.class, args);
+	}
+
+}
---
+++ simplecrudrest/simplecrudrest/src/test/java/com/fullstack/backend/api/SimplecrudrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrudrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
