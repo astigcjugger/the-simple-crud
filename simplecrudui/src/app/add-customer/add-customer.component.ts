3327c235bd12d77e5bbcea0cf1b69276441d0fc2
TERRENAL, GERRY (gt2519) <gt2519@us.att.com>
2020-02-13 22:37:24 -0800

    v1.5 - Updates to Add Customer with office assignments.

--- simplecrudui/src/app/add-customer/add-customer.component.ts
+++ simplecrudui/src/app/add-customer/add-customer.component.ts
@@ -14,12 +14,15 @@ import { Address } from '../Address';
 export class AddCustomerComponent implements OnInit {
 
   customer: Customer = new Customer;
-  submitted = false;
-  createmsg = 'Successfully submitted. Added new customer.';
+  submitted: boolean = false;
+  createmsg: string = 'Successfully submitted. Added new customer.';
   address: Address = new Address;
+  custOffices: Office[] = new Array<Office>();
   offices: Office[] = new Array<Office>();
-  hidePartOne = false;
-  hidePartTwo = true;
+  hidePartOne: boolean = false;
+  hidePartTwo: boolean = true;
+  selectedOfficeIds: Map<number, Office>;
+  mapOfOffices: Map<number, Office>;
 
   constructor(private customerService: CustomerService,
     private officeService: OfficeService,
@@ -29,11 +32,59 @@ export class AddCustomerComponent implements OnInit {
     this.officeService.getOfficeList().subscribe(data => {
       console.log(data);
       this.offices = data;
+      this.loadMapOfOffices();
     }, error => console.log(error));
+  }
+
+  loadMapOfOffices() {
+//    var tempOffices = JSON.parse(JSON.stringify(this.offices));
+//    this.mapOfOffices = new Map<String, Office>();
+//    var counts: number = this.offices.length;
+//    for (var x = 0; x < counts; x++) {
+//      var anOffice = tempOffices.pop();
+//      console.log('Office being loaded to map: ' + anOffice);
+///      this.mapOfOffices.set('' + anOffice.id, anOffice);
+//    }
+
+    console.log('Contents of Offices[]');
+    console.log(this.offices);
+    this.mapOfOffices = new Map<number, Office>();
+    for (let office of this.offices) {
+      console.log('Office entry: ');
+      console.log(office);
+      this.mapOfOffices.set(office.id, office);
+    }
+
+    console.log('Contents of mapOfOffices: Map()...');
+    console.log(this.mapOfOffices);
+    this.selectedOfficeIds = new Map<number, Office>();
+  }
+
+  onChange(officeId: number, isChecked: boolean) {
+    console.log('OfficeId: ' + officeId);
+    console.log('Is checked? ' + isChecked);
+    console.log(officeId);
+    if (isChecked) {
+      this.selectedOfficeIds.set(officeId, this.mapOfOffices.get(officeId));
+      console.log('Contents of officeId: ' + officeId);
+      console.log(this.mapOfOffices.get(officeId));
+    } else {
+      this.selectedOfficeIds.delete(officeId);
+    }
   }
 
   save() {
     this.customer.address = this.address;
+    console.log('SelectedOfficeIds...');
+    console.log(this.selectedOfficeIds);
+    let tempOffices = this.selectedOfficeIds.values();
+    for (let anOffice of tempOffices) {
+      console.log('Office being selected and loaded...')
+      console.log(anOffice);
+      this.custOffices.push(anOffice);
+    }
+
+    this.customer.offices = this.custOffices;
     this.customerService.addCustomer(this.customer).subscribe(
       data => console.log(data), 
       error => {
